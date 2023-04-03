9aed64c3e9faa9ba65e1b62e851c0dc156768507
utku <mehmetutkubalikci@gmail.com>
2023-04-03 23:20:13 +0300

    post

---
+++ src/components/post/Post.js
@@ -0,0 +1,45 @@
+import React, {useState,useEffect} from "react";
+import ReactDOM from "react-dom";
+
+function Post(){
+    const [error,setError] = useState(null);
+    const [isLoaded, setIsLoaded ] = useState(null);
+    const [postList, setPostList ] = useState([]);
+
+    useEffect(() => {
+        fetch("/posts")
+        .then(res => res.json())
+        .then(
+            (result) => {
+                setIsLoaded(true);
+                setPostList(result);
+            },
+            (error) => {
+                setIsLoaded(true);
+                setError(error);
+            }
+        )
+
+
+    }, [])
+
+    if (error){
+        return <div>Error!</div>;
+    }
+    else if (!isLoaded){
+        return <div>Loading...</div>;
+    }
+    else {
+        return(
+            <ul>
+                {postList.map(post => (
+                    <li>
+                        Title : {post.title} Text : {post.text}
+                    </li>
+                ))}
+            </ul>
+        );
+    }
+}
+
+export default Post;
